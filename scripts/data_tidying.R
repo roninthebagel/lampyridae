8ef6ee9b18d04d4a72ae9c95cb1b8e56e166f854
roninthebagel <ronin@run2online.com>
2025-10-21 11:47:01 +0100

    looking at descriptive data
    looking at data structure using descriptive stats

--- scripts/data_tidying.R
+++ scripts/data_tidying.R
@@ -1,3 +1,16 @@
 # reading csv file
 lampyridae <- read.csv("data/lampyridae_counts.csv")
 
+### --- looking at descriptive data --- ###
+
+# looking at the data structure
+glimpse(lampyridae)
+
+# generating key initial summary statistics for each variable
+# also includes frequencies for character variables to quickly understand data distribution
+summary(lampyridae)
+
+# per column summaries
+# includes data types, missing data, and statistics
+skimr::skim(lampyridae)
+
