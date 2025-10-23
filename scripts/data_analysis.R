d3b6987c909c90928706d41d9264887f3ab8b7eb
roninthebagel <ronin@run2online.com>
2025-10-23 11:16:42 +0100

    reshuffled comments
    reshuffled comments on data analysis file

--- scripts/data_analysis.R
+++ scripts/data_analysis.R
@@ -11,10 +11,14 @@ lampyridae_alpha_shannon <-
 ### --- beta diversity --- ###
 
 # generating stats for lampyridae vs habitat and count type
-lampyridae_summary_beta <- lampyridae_complete |> # stating new save data frame name and required data set
-  group_by(count_type, habitat) |> # stating which columns to include in analysis, and what order to appear on table
-  summarise(mean = mean(g5_lampyridae, na.rm =T), # stating i want to calculate means using values from g5_lampyridae column
-            sd = sd(g5_lampyridae, na.rm=T)) # stating i want to calculate standard deviations using values from g5_lampyridae column 
+lampyridae_summary_beta <- lampyridae_complete |> 
+  # stating new save data frame name and required data set
+  group_by(count_type, habitat) |> 
+  # stating which columns to include in analysis, and what order to appear on table
+  summarise(mean = mean(g5_lampyridae, na.rm =T), 
+            # stating i want to calculate means using values from g5_lampyridae column
+            sd = sd(g5_lampyridae, na.rm=T)) 
+            # stating i want to calculate standard deviations using values from g5_lampyridae column 
 
 # saving summmary table
 write.csv(lampyridae_summary_beta, file = "data/lampyridae_summary_beta.csv")
